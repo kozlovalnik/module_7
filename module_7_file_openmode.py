1e0f2d629b7a10dc634ff34d209b396684b9851a
Александр <kozlov.alexander.63@gmail.com>
2024-11-08 15:29:46 +0300

    Add files via upload

---
+++ module_7_file_openmode.py
@@ -0,0 +1,46 @@
+
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self) -> str:
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'product.txt'
+        try:
+            file = open(self.__file_name, 'r')
+            file.close()
+        except FileNotFoundError:
+            file = open(self.__file_name, 'w')
+            file.close()
+
+    def get_products(self) -> str:
+        file = open(self.__file_name, 'r')
+        products_str = file.read()
+        file.close()
+        return products_str
+
+    def add(self, *products: Product):
+        for p in products:
+            if f'{p.name}, {p.weight}' not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(f'{p.name}, {p.weight}, {p.category}\n')
+                file.close()
+            else:
+                print(f'Продукт {p.name} уже есть в магазине')
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
