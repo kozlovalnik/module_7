6274089b7b3c24b824b716dfb5ba8aac1be1478c
Александр <kozlov.alexander.63@gmail.com>
2024-11-09 10:10:56 +0300

    Add files via upload

---
+++ module_7_file_positioning.py
@@ -0,0 +1,21 @@
+
+def custom_write(file_name: str, strings: list) -> dict:
+    file = open(file_name, 'w')
+    string_positions ={}
+    for i in range(len(strings)):
+        string_positions[(i, file.tell())] = strings[i]
+        file.write(strings[i] + '\n')
+    file.close()
+    return string_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
+
