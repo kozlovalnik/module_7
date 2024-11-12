bbae82b38ec5f96cf74c588ab26c9b46c1127935
Александр <kozlov.alexander.63@gmail.com>
2024-11-12 17:00:02 +0300

    Add files via upload

--- module_7_os.py
+++ module_7_os.py
@@ -1,14 +1,12 @@
 import os
 import time
-from os.path import pardir
 
 for root, dirs, files in os.walk('.'):
     for file in files:
-        filepath = os.getcwd()
-        fullpath = os.path.join(os.getcwd(), file)
-        filetime = os.path.getmtime(file)
+        fullpath = os.path.join(root, file)
+        filetime = os.path.getmtime(fullpath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
+        filesize = os.path.getsize(fullpath)
         parent_dir = os.path.pardir
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize}, Время изменения {formatted_time}, Родительская директория {parent_dir}')
+        print(f'Обнаружен файл: {file}, Путь: {root}, Размер: {filesize}, Время изменения {formatted_time}, Родительская директория {parent_dir}')
     break
